063d42166d4c861454418975b794dcb46c8198bb
Atsushi <newhandarky@gmail.com>
2025-02-13 11:09:04 +0800

    關閉所有res.render()

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var indexRouter = express.Router();
 
 /* GET home page. */
 indexRouter.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
 });
 
 export default indexRouter;
